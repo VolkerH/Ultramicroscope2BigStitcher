4bb543c4300eb5fd998d2db7dffa5310d2a82f98
Volker Hilsenstein <volker.hilsenstein@gmail.com>
2019-10-24 14:40:25 +1100

    first commit of code from private repo

---
+++ um2bs/process_um_folder.py
@@ -0,0 +1,320 @@
+# Create a big stitcher file from a scan
+# with a LaVision Biotec Ultramicroscope (SPIM)
+#
+# License BSD-3
+# Volker
+# .Hilsenstein @ monash
+# .edu
+
+import re
+import pathlib
+import numpy as np
+import pandas as pd
+import time
+import npy2bdv
+import skimage.io
+from functools import partial
+from concurrent.futures import ThreadPoolExecutor
+from typing import Union, List, Dict
+import tifffile
+import warnings
+
+
+def readstack(files: List[str], convertto=None) -> np.ndarray:
+    """Reads list of files as stack (using multiple Threads) with optional typeconversion
+    
+    Parameters
+    ----------
+    files : List[str]
+        list of filenames
+    convertto : [type], optional
+        type to convert to, by default None
+    
+    Returns
+    -------
+    np.ndarray
+        stack that has been read
+    """
+
+    def _imread(input):
+        print(f"reading {input}")
+        tmp = tifffile.imread(input, multifile=False)
+
+        print(tmp.shape)
+        if convertto is not None:
+            return tmp.astype(convertto)
+        else:
+            return tmp
+
+    with ThreadPoolExecutor() as p:
+        images = list(p.map(_imread, files))
+    return np.array(images)
+
+
+def conv_strvector_to_np(strvalue: str) -> np.ndarray:
+    """converts a string vector such as '(12.0, 13.0, 14.0)' into 
+       a numpy array. To be used as a converter function with 
+     np.read_csv
+    """
+    strvalue = re.sub("[\[\]\(\)]", "", strvalue)
+    npar = np.fromstring(strvalue, sep=",", dtype=np.float)
+    return npar
+
+
+class um_mosaic_folder:
+    def __init__(self, foldername: Union[pathlib.Path, str], regexes: Dict[str, str]):
+        """init ultramicroscope mosaic folder object
+        
+        Parameters
+        ----------
+        foldername : str or pathlib.Path
+            Folder of Ultramicroscope images to be processed
+        regexes : Dict[str:str]
+            Dictionary of regular expression strings used to
+            filter the files and extract metadata from the filenames
+
+            The following keys are expected:
+            'filewhitelist' files to be considered must match this regular expression, e.g. '.*.tif'
+            'Z' regular expression to extract Z slice number
+            'ch' regular expression to extract channel number
+            'illu' regular expression to extract illumination direction
+        """
+        # Take the provided foldername and anlyze the contents
+        self.umpath = pathlib.Path(foldername)
+        self.regexes = regexes
+        assert self.umpath.exists
+        self.update()
+
+    def update(self):
+        self._read_tile_info()
+        self._find_imfiles()
+
+    def _find_imfiles(self):
+        """ looks for all files in self.umpath 
+
+        filters files to retain those that match "filewhitelist" from self.regexes
+
+        Uses the other regexes  in self.regexes to populate metadata 
+        """
+        self.tiffiles = list(self.umpath.glob("*"))
+        regex = re.compile(self.regexes["filewhitelist"])
+        self.tiffiles = list(filter(regex.search, map(str, self.tiffiles)))
+        self.df = pd.DataFrame(self.tiffiles, columns=["pathname"])
+        self.df["filename"] = self.df["pathname"].apply(lambda f: pathlib.Path(f).name)
+
+        def _extraxt_first_match_with_regex(regex, teststring):
+            try:
+                return regex.findall(teststring)[0]
+            except:
+                return ""
+
+        for regexname in self.regexes.keys():
+            if regexname != "filewhitelist":
+                print(f"Applying regex {regexname} to filenames.")
+                regex = re.compile(self.regexes[regexname])
+                _extract = partial(_extraxt_first_match_with_regex, regex)
+                self.df[regexname] = self.df["filename"].apply(_extract)
+        # for column in ["Z","ch"]:
+        #    self.df[column] = pd.to_numeric(self.df[column])
+
+        self.df = pd.merge(self.df, self.tiles, on="filename")
+
+        # In order to identify Z-stacks we add a column with the filename of the first Z slice
+        unique_z = self.df["Z"].unique()
+        # numeric_unique_z  = list(map(int, unique_z))
+        unique_z = sorted(unique_z, key=int)
+        print(f"first z slice is {unique_z[0]}")
+        _replacer = partial(re.sub, self.regexes["Z"], unique_z[0])
+        self.df["first_Z"] = self.df["filename"].apply(_replacer)
+
+        self.df["Znumeric"] = self.df["Z"].apply(int)
+        self.df = self.df.sort_values("Znumeric")
+        print(self.df)
+        print(f'nr of unique Z: {len(self.df["Z"].unique())}')
+        print(f'nr of unique Illu: {len(self.df["illu"].unique())}')
+        print(f'nr of uniqu ch: {len(self.df["ch"].unique())}')
+
+    def _read_tile_info(self):
+        tilefile = self.umpath / "tiles.txt"
+        # print(str(tilefile))
+        if tilefile.exists():
+            print("Trying to read tiles.txt file")
+            self.tiles = pd.read_csv(
+                str(tilefile),
+                sep=";",
+                skiprows=[0],
+                names=["filename", "unknown", "stagexyz"],
+                converters={"stagexyz": conv_strvector_to_np},
+            )
+        else:
+            print("Tiles.txt not found")
+
+    def _generate_project_folder(self, basefolder: str, projtype: str):
+        projectfolder = pathlib.Path(basefolder) / pathlib.Path(projtype)
+        projectfolder.mkdir(parents=True, exist_ok=True)
+        return str(projectfolder / "dataset.h5")
+
+    def generate_big_stitcher(
+        self,
+        outfolder_base: str,
+        projected: bool = True,
+        volume: bool = True,
+        xyspacing: float = 1.0,
+        zspacing: float = 1.0,
+        project_func=np.max,
+        direction_x: int = 1,
+        direction_y: int = -1,
+    ):
+        """Generate a big stitcher project (.xml/h5)
+        
+        Parameters
+        ----------
+        outfolder_base : str
+            base folder for the output
+        projected : bool, optional
+            if True, create a project based on z-projected tiles, by default True
+        volume : bool, optional
+            if True create a project for the full volumes, by default True
+        xyspacing : float, optional
+            pixel spacing in x/y in um/pix, by default 1.0
+        zspacing : float, optional
+            pixel spacing in z in um/pix, by default 1.0
+        project_func : [type], optional
+            projection function for z-stacks if projected, by default np.max
+        direction_x : int, optional
+            can be used to flip coordinate axes should be 1 or -1, by default 1
+        direction_y : int, optional
+            as above, by default -1
+        """
+
+        if not (projected or volume):
+            print("Neither 2D nor 3D project generation selected. Nothing to do.")
+            return
+
+        assert len(self.df) > 0, "No files found"
+        print(f"Zspacing: {zspacing}")
+        print(f"XYspacing: {xyspacing}")
+
+        # get unique channels and illuminations
+        # the index into these lists will be used to identify the dataset
+        channels = list(
+            map(str, self.df["ch"].unique())
+        )  # converting to string fixes problems with nan
+        illuminations = list(map(str, self.df["illu"].unique()))
+
+        # Group by stack
+        grouped_stacks = self.df.groupby("first_Z")
+        ntiles: int = len(grouped_stacks)
+        nchannels: int = len(channels)
+        nillu: int = len(illuminations)
+        print(f"Processing {ntiles} tiles.")
+        affine_matrix_template = np.array(
+            ((1.0, 0.0, 0.0, 0.0), (0.0, 1.0, 0.0, 0.0), (0.0, 0.0, 1.0, 0.0))
+        )
+
+        if projected:
+            h5_proj_name: str = self._generate_project_folder(outfolder_base, "projected")
+            bdv_proj_writer = npy2bdv.BdvWriter(
+                h5_proj_name,
+                nchannels=nchannels,
+                nilluminations=nillu,
+                ntiles=ntiles,
+                subsamp=((1, 1, 1), (1, 2, 2), (1, 4, 4), (1, 8, 8), (1, 16, 16)),
+                blockdim=((1, 64, 64),),
+                compression="gzip",
+            )
+
+        if volume:
+            h5_vol_name: str = self._generate_project_folder(outfolder_base, "volume")
+            bdv_vol_writer = npy2bdv.BdvWriter(
+                h5_vol_name,
+                nchannels=nchannels,
+                nilluminations=nillu,
+                ntiles=ntiles,
+                subsamp=(
+                    (1, 1, 1),
+                    (1, 2, 2),
+                    (1, 4, 4),
+                    (1, 8, 8),
+                    (2, 16, 16),
+                    (4, 32, 32),
+                ),
+                blockdim=(
+                    (64, 64, 64),
+                    (64, 64, 64),
+                    (64, 64, 64),
+                    (64, 64, 64),
+                    (32, 32, 32),
+                    (16, 16, 16),
+                ),
+                compression="gzip",
+            )
+
+        for tile_nr, (grname, group) in enumerate(grouped_stacks):
+            print(f"Processing {tile_nr+1} out of {ntiles}:")
+            stack = readstack(group["pathname"].values, convertto=np.int16)
+            # clip values in order to not make Big Stitcher crash
+            # this may no longer be necessary with a more recent
+            # versoin of npy2bv but have not tested (TODO)
+            if np.any(stack > 32767) or np.any(stack < 0):
+                warnings.warn(
+                    "greyvalues outside supported range detected. Will be clipped !",
+                    UserWarning,
+                )
+                np.clip(stack, 0, 32767, out=stack)
+
+            print("finished reading stack")
+            xyz = group["stagexyz"].values[0]
+            print(f"xyz is {xyz}")
+            affine = affine_matrix_template.copy()
+
+            # Explanation for formula below:
+            # Stage position in metadata appears to be in units of metres (m)
+            # PhysicalSize appears to be micrometers per voxel (um/vox)
+            # therefore for the stageposition in voxel coordinates we need to
+            # scale from meters to um (factor 1000000) and then divide by um/vox
+            # the direction vectors should be either 1 or -1 and can be used
+            # to flip the direction of the coordinate axes.
+            affine[1, 3] = xyz[1] / xyspacing * direction_y
+            affine[0, 3] = xyz[0] / xyspacing * direction_x
+
+            ch = str(group["ch"].values[0])
+            illu = str(group["illu"].values[0])
+            ch_index = channels.index(ch)
+            illu_index = illuminations.index(illu)
+
+            if volume:
+                bdv_vol_writer.append_view(
+                    stack,
+                    time=0,
+                    channel=ch_index,
+                    illumination=illu_index,
+                    m_affine=affine,
+                    tile=tile_nr,
+                    name_affine=f"tile {tile_nr} translation",
+                    voxel_size_xyz=(xyspacing, xyspacing, zspacing),
+                    voxel_units="um",
+                    calibration=(1, 1, zspacing / xyspacing),
+                )
+            if projected:
+                outstack = np.expand_dims(project_func(stack, axis=0), axis=0)
+                bdv_proj_writer.append_view(
+                    outstack,
+                    time=0,
+                    channel=ch_index,
+                    illumination=illu_index,
+                    m_affine=affine,
+                    tile=tile_nr,
+                    name_affine=f"proj. tile {tile_nr} translation",
+                    # Projections are inherently 2D, so we just repeat the X voxel size for Z
+                    voxel_size_xyz=(xyspacing, xyspacing, xyspacing),
+                    voxel_units="um",
+                    # calibration=(1, 1, 1),
+                )
+
+        if projected:
+            bdv_proj_writer.write_xml_file(ntimes=1)
+            bdv_proj_writer.close()
+        if volume:
+            bdv_vol_writer.write_xml_file(ntimes=1)
+            bdv_vol_writer.close()
---
+++ um2bs/um2bs_gui.py
@@ -0,0 +1,187 @@
+# Simple GUI for process_um_folder functions
+# Allows converting acquisitions from Ultramicroscope to Big Stitcher projects
+#
+# License BSD-3
+# Author:
+# Volker.
+# Hilsenstein@
+# monash
+# edu
+
+from PyQt5 import QtWidgets, QtCore, QtGui
+from process_um_folder import um_mosaic_folder
+from background_worker import Worker, WorkerSignals
+import pathlib
+
+# To add progress bar https://riptutorial.com/pyqt5/example/29500/basic-pyqt-progress-bar
+# to add a waiting spinner (while looking for wells) https://gist.github.com/eyllanesc/1a09157d17ba13d223c312b28a81c320
+# don't block main event loop: https://www.learnpyqt.com/courses/concurrent-execution/multithreading-pyqt-applications-qthreadpool/
+
+
+class UltraMicroscopeToBigStitcherGUI(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super(UltraMicroscopeToBigStitcherGUI, self).__init__(parent)
+        self.processor = None
+        self.rootfolder = "C:/Users/Volker/Data/Ultra_Oct2019/ultra_microscope_minimal"
+        self.outfolder = ""
+        self.threadpool = QtCore.QThreadPool()
+        print("Multithreading with maximum %d threads" % self.threadpool.maxThreadCount())
+
+        self.layout = QtWidgets.QVBoxLayout()
+
+        # folders
+        self.inputFolderButton = QtWidgets.QPushButton("Select input folder")
+        self.selectedroot = QtWidgets.QLabel(self.rootfolder)
+        self.outputFolderButton = QtWidgets.QPushButton("Select output folder")
+        self.selectedoutput = QtWidgets.QLabel(self.outfolder)
+        # nr of files found info
+        self.nr_files_found = QtWidgets.QLabel("input folder not scanned yet")
+        # output option checkboxes
+        self.checkbox_2D = QtWidgets.QCheckBox("create 2D BDV file (max-projected Z)")
+        self.checkbox_2D.setChecked(True)
+        self.checkbox_3D = QtWidgets.QCheckBox("create 3D BDV file")
+        self.checkbox_3D.setChecked(False)
+        # regular expressions for file selection and metadata extraction
+        self.lineedit_re_filewhitelist = QtWidgets.QLineEdit()
+        self.lineedit_re_filewhitelist.setText(".*tif")
+        self.lineedit_re_Z = QtWidgets.QLineEdit()
+        self.lineedit_re_Z.setText("(?<=_C)\d+")  # https://regex101.com/r/lQfpkm/1/
+        self.lineedit_re_ch = QtWidgets.QLineEdit()
+        self.lineedit_re_ch.setText("(?<=channel)\d+")
+        self.lineedit_re_illu = QtWidgets.QLineEdit()
+        self.lineedit_re_illu.setText(
+            "(?<=_Ill)[\da-zA-Z]+"
+        )  # https://regex101.com/r/sk8w3u/1/
+
+        # voxel dimensions
+        self.lineedit_zspacing = QtWidgets.QLineEdit()
+        self.lineedit_zspacing.setText("1.00")
+        self.lineedit_zspacing.setValidator(QtGui.QDoubleValidator(0.0, 10000.0, 2))
+        self.lineedit_xyspacing = QtWidgets.QLineEdit()
+        self.lineedit_xyspacing.setText("1.00")
+        self.lineedit_xyspacing.setValidator(QtGui.QDoubleValidator(0.0, 10000.0, 2))
+        # List of tiles
+        self.listWidget = QtWidgets.QListWidget()
+        self.listWidget.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
+        self.listWidget.setGeometry(QtCore.QRect(10, 10, 211, 291))
+        self.startProcessingButton = QtWidgets.QPushButton("Process selected folders")
+        self.startProcessingButton.setEnabled(False)
+        # Make connections
+        self.listWidget.itemSelectionChanged.connect(self._checkProcessingButton)
+        self.inputFolderButton.clicked.connect(self.get_root_folder)
+        self.outputFolderButton.clicked.connect(self.get_output_folder)
+        self.startProcessingButton.clicked.connect(self.process)
+        # Assemble GUI elements into final layout
+
+        self.layout.addWidget(self.inputFolderButton)
+        self.layout.addWidget(QtWidgets.QLabel("Input folder:"))
+        self.layout.addWidget(self.selectedroot)
+        self.layout.addWidget(
+            QtWidgets.QLabel("Regular expression for files to consider")
+        )
+        self.layout.addWidget(self.lineedit_re_filewhitelist)
+        self.layout.addWidget(self.nr_files_found)
+        self.layout.addWidget(self.outputFolderButton)
+        self.layout.addWidget(QtWidgets.QLabel("Output folder:"))
+        self.layout.addWidget(self.selectedoutput)
+
+        self.layout.addWidget(QtWidgets.QLabel("Regular expression for Z slices <zpos>"))
+        self.layout.addWidget(self.lineedit_re_Z)
+        self.layout.addWidget(QtWidgets.QLabel("Regular expression for channels <ch>"))
+        self.layout.addWidget(self.lineedit_re_ch)
+        self.layout.addWidget(
+            QtWidgets.QLabel("Regular expression for illumination <illu>")
+        )
+        self.layout.addWidget(self.lineedit_re_illu)
+        self.layout.addWidget(self.checkbox_2D)
+        self.layout.addWidget(self.checkbox_3D)
+        self.layout.addWidget(QtWidgets.QLabel("Enter XY spacing in um/voxel:"))
+        self.layout.addWidget(self.lineedit_xyspacing)
+        self.layout.addWidget(QtWidgets.QLabel("Enter Z spacing in um/voxel:"))
+        self.layout.addWidget(self.lineedit_zspacing)
+
+        # self.layout.addWidget(QtWidgets.QLabel("Select the wells to process:"))
+        # self.layout.addWidget(self.listWidget)
+        self.layout.addWidget(self.startProcessingButton)
+
+        self.setLayout(self.layout)
+
+    def get_root_folder(self):
+        self.rootfolder = str(
+            QtWidgets.QFileDialog.getExistingDirectory(self, "Select Directory")
+        )
+        self.update_files()
+        self._trigger_update()
+
+    def get_output_folder(self):
+        self.outfolder = str(
+            QtWidgets.QFileDialog.getExistingDirectory(self, "Select Output Folder")
+        )
+        self.selectedoutput.setText(self.outfolder)
+        self._checkProcessingButton()
+
+    def process(self):
+        self.startProcessingButton.setEnabled(False)
+
+        worker = Worker(self._process)
+        worker.signals.finished.connect(self._checkProcessingButton)
+        self.threadpool.start(worker)
+
+    def _process(self, *args, **kwargs):
+
+        print(f"Input folder {self.rootfolder}")
+        print(f"Output folder {self.outfolder}")
+        print(f"2D projection {self.checkbox_2D.isChecked()}")
+        print(f"2D volume {self.checkbox_3D.isChecked()}")
+        print(f"xy scale um/pix {float(self.lineedit_xyspacing.text())}")
+        print(f"z scale um/pix {float(self.lineedit_zspacing.text())}")
+
+        self.processor.generate_big_stitcher(
+            outfolder_base=self.outfolder,
+            projected=self.checkbox_2D.isChecked(),
+            volume=self.checkbox_3D.isChecked(),
+            xyspacing=float(self.lineedit_xyspacing.text()),
+            zspacing=float(self.lineedit_zspacing.text()),
+        )
+
+    def _checkProcessingButton(self):
+        # if (
+        #    self.listWidget.count() > 0
+        #    and self.outfolder != ""
+        #    and len(self._get_selected_indices()) > 0
+        # ):
+        if self.outfolder != "" and self.rootfolder != "":
+            self.startProcessingButton.setEnabled(True)
+        else:
+            self.startProcessingButton.setEnabled(False)
+
+    def _trigger_update(self):
+        worker = Worker(self.update_files)
+        # worker.signals.result.connect(self.print_output)
+        worker.signals.finished.connect(self._checkProcessingButton)
+        # worker.signals.progress.connect(self.progress_fn)
+        print("starting worker to find files")
+        self.threadpool.start(worker)
+
+    def update_files(self, *args, **kwargs):
+        print("in update wells")
+        self.selectedroot.setText(self.rootfolder)
+        # self.listWidget.clear()
+        print("initializing Matrix processor")
+        res = {}
+        for re_name in ["filewhitelist", "Z", "ch", "illu"]:
+            widget = eval(f"self.lineedit_re_{re_name}")
+            res[re_name] = widget.text()
+        self.nr_files_found.setText("Looking for files ...")
+        self.processor = um_mosaic_folder(self.rootfolder, res)
+
+        print(self.processor.df)
+        self.nr_files_found.setText(f"{len(self.processor.df)} files found")
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    form = UltraMicroscopeToBigStitcherGUI()
+    form.show()
+    app.exec_()
